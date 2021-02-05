d8fb4322cd8b2f5e4fdddab04c720a5bc56a8436
felipeDesenvolvedor <46979397+felipeDesenvolvedor@users.noreply.github.com>
2021-02-05 18:02:43 -0300

    Form

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,21 @@
 const getUser = (props) => {
-
-    (function(){
-        clearTimeout(this.interval);
-        this.interval = setTimeout(search, 1000);
-    })();
-
     const search = () => {
         let user = props.target.value;
 
-        let userGithub;
-        userGithub = fetch(`https://api.github.com/users/${user}`).then(resposta => {
+        return fetch(`https://api.github.com/users/${user}`).then(resposta => {
             return resposta.json();
         }).then(json => {
-            console.log(json);
+            return json;
         });
-    }    
+    }
+    
+    (function(){
+        var interval;
+        clearTimeout(interval);
+        interval = setTimeout(search, 1000);
+    })();
+
+    return search;
 }
 
 export {getUser};
